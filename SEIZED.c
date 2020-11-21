5e28797d12cab2e09e4c138dc3c2b9e2a1090f45
Agung Krisna <34364395+Agung-Krisna@users.noreply.github.com>
2020-11-21 12:28:21 +0000

    SEIZED - Non Playable [STABLE]
    This is a version to show the image "Coming Soon", and already configured to exit when the exit button is pressed.

--- SEIZED.c
+++ SEIZED.c
@@ -1,14 +1,29 @@
+/*
+HELLO DEVS,
+In order to understand and contribute to the development of the game, 
+here are the pre-requisite:
+=====================================================
+You need to understand C programming language and some of its core concept like if-else, structs, etc.
+You need to understand how SDL works, because this would be created mainly with SDL.
+You need to know how to do comments on your code, because not everyone is you.
+You need to be able how to troubleshoot your own code, don't push code that is not done yet!
+You need to state what kind of changes that you made in the pull requests.
+You need to understand that game programming is not easy!
+****************************************************
+Keep Coding for the Win!
+*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <SDL2/SDL.h>
 #include <time.h>
+#include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
 int main(int argc, char *argv[]){
     char errors[100];
+    time_t now;
+    int running = 1;
     FILE *ferrors;
     ferrors = fopen("errors/errors.txt", "a+");//file I/O
-    time_t now;
     time(&now);//giving the time so that we know precisely at what time our program failed.
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
         printf("Error initializing the game. The cause is %s\n", SDL_GetError());
@@ -28,7 +43,7 @@ int main(int argc, char *argv[]){
                                 720,                     //height (pixels)
                                 0);                      //flags
     
-    Uint32 render_flags = SDL_RENDERER_ACCELERATED;
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED;//type render_flags to set the renderer so that it would use the computer VRAM.
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, render_flags);//making the renderer
     if(!renderer)
     {
@@ -49,7 +64,39 @@ int main(int argc, char *argv[]){
         SDL_Quit();
         return EXIT_FAILURE;
     }
-    while (1);//while true (looping forever)
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, image); //making the texture so that the image is loaded to the memory
+    SDL_FreeSurface(image);//freeing the surface because it already converted into a texture.
+    if(!tex)
+    {
+        printf("Error creating texture\n");
+        SDL_DestroyWindow(window);
+        SDL_DestroyRenderer(renderer);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_RenderClear(renderer); //clearing the renderer so that it would produce black screen instead of crashing.
+
+    /*Drawing the image into the window with Double Buffer Technique*/
+    SDL_RenderCopy(renderer, tex, NULL, NULL); //doing the processing in the background
+    SDL_RenderPresent(renderer);//presenting the image to the user.
+
+    SDL_Event exit; //creating an event called exit.
+
+    while(running == 1)//I WISH WE HAVE BOOLEAN HERE, BUT WE MUST ADAPT!
+    {
+        while(SDL_PollEvent(&exit) != 0)//poll event means that we will record the event that is done by the user.
+        {
+            if(exit.type == SDL_QUIT)//SDL_QUIT is an Enum, while SDL_Quit is a function.
+                running = 0;//changing the value of running.
+        }
+        SDL_UpdateWindowSurface(window);//updating the window in each turn
+    }
+    
+    /*SELF EXPLANATORY*/
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
